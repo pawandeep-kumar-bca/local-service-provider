18f65752ac895dedd6111076a2114ef56890abc0
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-04-10 11:18:16 +0530

    style:  user dashboard UI style

--- frontend/src/pages/user/UserDashboard.jsx
+++ frontend/src/pages/user/UserDashboard.jsx
@@ -1,3 +1,5 @@
+import { BiBookAdd } from "react-icons/bi";
+import { FaCheckCircle, FaStar } from "react-icons/fa";
 
 const UserDashboard = () => {
   return (
@@ -16,10 +18,28 @@ const UserDashboard = () => {
         </div>
         <img src="/assets/project.png" alt="image" className="w-[15%] hidden md:block"/>
       </div>
-     <div>
-        <div className="w-[20%] shadow-[0_0_15px_rgba(0,0,0,0.45)] p-5 rounded-lg mt-6">
-
+     <div className="grid grid-cols-2 gap-2 mt-8 mb-6">
+        <div className="shadow-[0_0_15px_rgba(0,0,0,0.45)] p-5 rounded-lg  flex items-start gap-3">
+        <BiBookAdd className="text-3xl text-danger"/>
+        <div className="flex flex-col items-center"><h1 className="text-2xl font-bold">4</h1>
+        <h2 className="text-sm font-bold">Total Booking</h2></div>
      </div>
+        <div className="shadow-[0_0_15px_rgba(0,0,0,0.45)] p-5 rounded-lg flex items-start gap-6 ">
+         <FaCheckCircle className="text-2xl text-success" />
+        <div className="flex w-[50%] flex-col items-center"><h1 className="text-2xl font-bold">4</h1>
+        <h2 className="text-sm font-bold">Active</h2></div>
+     </div>
+      <div className="shadow-[0_0_15px_rgba(0,0,0,0.45)] p-5 rounded-lg flex items-start gap-4">
+        <FaCheckCircle className="text-2xl text-primary" />
+        <div className="flex w-[50%] flex-col items-center"><h1 className="text-2xl font-bold">4</h1>
+        <h2 className="text-sm font-bold">Completed</h2></div>
+     </div>
+        <div className="shadow-[0_0_15px_rgba(0,0,0,0.45)] p-5 rounded-lg flex items-start gap-4">
+        <FaStar className="text-2xl text-warning" />
+        <div className="flex w-[50%] flex-col items-center"><h1 className="text-2xl font-bold">4</h1>
+        <h2 className="text-sm font-bold">Reviewed</h2></div>
+     </div>
+       
      </div>
     </div>
   );
