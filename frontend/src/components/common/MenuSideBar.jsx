5e29e0d5b3301b62e7213ee328499e75e061a1ed
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-04-10 09:51:01 +0530

    style : style responsive navbar and menubar together UI page

--- frontend/src/components/common/MenuSideBar.jsx
+++ frontend/src/components/common/MenuSideBar.jsx
@@ -14,21 +14,29 @@ const MenuSideBar = ({ role }) => {
         : role === "admin"
           ? adminMenu
           : publicMenu;
-  return <div className="w-full md:w-[20%] h-screen bg-red-100 flex flex-col items-center pt-20 md:pt-2">
-    <img
-    src="/assets/logo.png"
-    alt="logo"
-    className="w-[3rem] hidden md:block"
-  />
 
-    <div className="w-full h-[1px] bg-muted md:flex hidden"></div>
-    {menu.map((item,idx)=>(
-        <div key={idx} className="w-full flex items-center  gap-3 md:px-4">
+  return (
+    <div className="w-full md:w-[20%] h-screen bg-green-100 flex flex-col items-center absolute pt-20 top-0  md:pt-2">
+      <img
+        src="/assets/logo.png"
+        alt="logo"
+        className="w-[3rem] hidden md:block"
+      />
+
+      <div className="w-full h-[1px] bg-muted md:flex hidden"></div>
+      {menu.map((item, idx) => (
+        <div key={idx} className="w-full flex items-center justify-center gap-5 md:px-4">
           <div className="hidden md:flex text-2xl ">{item.icon}</div>
-          <Link to={item.path} className="text-xl font-semibold mb-2 md:text-lg">{item.name}</Link>
-          </div>
-    ))}
-  </div>;
+          <Link
+            to={item.path}
+            className="text-xl font-semibold mb-2 md:text-lg"
+          >
+            {item.name}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MenuSideBar;
--- frontend/src/components/common/NavBar.jsx
+++ frontend/src/components/common/NavBar.jsx
@@ -1,46 +1,63 @@
-
-import { IoMdNotifications } from "react-icons/io";
+import { IoMdClose, IoMdNotifications } from "react-icons/io";
 import { IoMenu, IoSearch } from "react-icons/io5";
+import MenuSideBar from "./MenuSideBar";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [openMenuBar, setOpenMenuBar] = useState(false);
+
+  const menuBar = () => {
+    if (!openMenuBar) {
+      setOpenMenuBar(true);
+    } else {
+      setOpenMenuBar(false);
+    }
+  };
   return (
-    <div className="w-full bg-red-100 h-[5.3rem] flex items-center justify-between px-2 md:px-5 overflow-hidden ">
+    <>
+      <div className="w-full bg-red-100 h-[5.3rem] flex items-center justify-between px-2 md:px-5 overflow-hidden ">
+        {/* LOGO */}
+        <img src="/assets/logo.png" alt="logo" className="w-[3rem] md:hidden" />
 
-  {/* LOGO */}
-  <img
-    src="/assets/logo.png"
-    alt="logo"
-    className="w-[3rem] md:hidden"
-  />
+        {/* SEARCH */}
+        <div className="flex items-center gap-2 border border-muted rounded-lg py-1 md:py-2 px-2 flex-1  mx-4 ">
+          <IoSearch className="text-xl" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="text-sm w-full outline-none "
+          />
+        </div>
 
-  {/* SEARCH */}
-  <div className="flex items-center gap-2 border border-muted rounded-lg py-1 md:py-2 px-2 flex-1  mx-4 ">
-    <IoSearch className="text-xl"/>
-    <input
-      type="text"
-      placeholder="Search..."
-      className="text-sm w-full outline-none "
-    />
-  </div>
+        {/* RIGHT SIDE */}
+        <div className="flex items-center gap-3">
+          <div className="relative cursor-pointer">
+            <IoMdNotifications className="text-xl md:text-2xl" />
+            <div className="absolute -top-2 -right-2 bg-gray-500 text-white rounded-full px-1 text-[10px]">
+              0
+            </div>
+          </div>
 
-  {/* RIGHT SIDE */}
-  <div className="flex items-center gap-3">
-    <div className="relative cursor-pointer">
-      <IoMdNotifications className="text-xl md:text-2xl" />
-      <div className="absolute -top-2 -right-2 bg-gray-500 text-white rounded-full px-1 text-[10px]">
-        0
+          <img
+            src="/assets/profile.png"
+            alt="profile"
+            className="hidden md:flex md:w-14 md:h-14 rounded-full object-cover cursor-pointer"
+          />
+          {openMenuBar ? (
+            <IoMdClose
+              className="text-4xl z-10 mr-4 mt-4 cursor-pointer md:hidden"
+              onClick={menuBar}
+            />
+          ) : (
+            <IoMenu
+              className="text-3xl cursor-pointer md:hidden"
+              onClick={menuBar}
+            />
+          )}
+        </div>
       </div>
-    </div>
-
-    <img
-      src="/assets/profile.png"
-      alt="profile"
-      className="hidden md:flex md:w-14 md:h-14 rounded-full object-cover cursor-pointer"
-    />
-    <IoMenu  className="text-3xl cursor-pointer md:hidden"/>
-  </div>
-
-</div>
+      {openMenuBar && <MenuSideBar />}
+    </>
   );
 };
 
