0e6ee3b5a7dd39611db6afa99a38ba7046226264
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-04-12 09:17:16 +0530

    style: payment history list responsive style

--- frontend/src/pages/user/PaymentHistory.jsx
+++ frontend/src/pages/user/PaymentHistory.jsx
@@ -4,16 +4,16 @@ import Button from "../../components/common/Button";
 const PaymentHistory = () => {
   return (
     <div>
-      <div className="p-4 shadow-[0_0_30px_rgba(0,0,0,0.40)]">
-        <div className="flex md:flex-row flex-col md:justify-between md:items-center p-7">
-          <h1 className="text-2xl font-semibold mt-3
-          mb-3 md:text-3xl">Payment History</h1>
+      <div className=" shadow-[0_0_30px_rgba(0,0,0,0.40)]">
+        <div className="flex md:flex-row flex-col md:justify-between md:items-center md:px-7 px-2 md:pt-9 py-3 bg-muted/20 border-b">
+          <h1 className="text-2xl font-semibold 
+          md:mb-3 mb-4 md:text-3xl">Payment History</h1>
           <div className="flex gap-2 border border-muted rounded-lg py-1  px-2 items-center mb-4">
             <IoSearchOutline className="text-xl"/>
             <input type="text" placeholder="Search" className="w-full outline-0 text-xl"/>
           </div>
         </div>
-        <div className="grid md:grid-cols-7 gap-4 ml-15">
+        <div className="grid md:grid-cols-7 grid-cols-5  gap-4 md:px-15 px-1 py-4 md:py-5">
           <Button color="success">All</Button>
           <Button color="white">Completed</Button>
           <Button color="white">Refunds</Button>
@@ -21,8 +21,8 @@ const PaymentHistory = () => {
           <Button color="white">Failed</Button>
         </div>
         <div>
-          <div className="py-3">
-            <ul className="grid md:grid-cols-6 grid-cols-4 text-lg font-semibold   justify-items-center items-center ">
+          <div className="py-3 bg-muted/20 border-b border-t border-muted">
+            <ul className="grid md:grid-cols-6 grid-cols-4 text-lg md:text-xl font-semibold   justify-items-center items-center ">
               <li>Provider</li>
               <li>Date</li>
               <li className="hidden md:flex">Payment ID</li>
@@ -31,7 +31,9 @@ const PaymentHistory = () => {
               <li>Action</li>
             </ul>
           </div>
-          <div className="grid md:grid-cols-6 grid-cols-4 text-sm font-semibold text-muted  justify-items-center  items-center">
+         <div>
+          <div>
+           <div className="grid md:grid-cols-6 grid-cols-4 text-sm md:text-lg font-semibold text-muted  justify-items-center  items-center md:py-3 py-2">
             <h2>Aman Gupta</h2>
             <p>21 May 2024</p>
             <p className="hidden md:flex">#TXM5030</p>
@@ -39,7 +41,76 @@ const PaymentHistory = () => {
             <h3>₹ 200</h3>
             <Button color="blue">View</Button>
           </div>
+          <div className="w-full h-[1px] bg-muted my-3"></div>
+           </div>
+          <div>
+           <div className="grid md:grid-cols-6 grid-cols-4 text-sm md:text-lg font-semibold text-muted  justify-items-center  items-center md:py-3 py-2">
+            <h2>Aman Gupta</h2>
+            <p>21 May 2024</p>
+            <p className="hidden md:flex">#TXM5030</p>
+            <Button className="hidden md:flex">Completed</Button>
+            <h3>₹ 200</h3>
+            <Button color="blue">View</Button>
+          </div>
+          <div className="w-full h-[1px] bg-muted my-3"></div>
+           </div>
+          <div>
+           <div className="grid md:grid-cols-6 grid-cols-4 text-sm md:text-lg font-semibold text-muted  justify-items-center  items-center md:py-3 py-2">
+            <h2>Aman Gupta</h2>
+            <p>21 May 2024</p>
+            <p className="hidden md:flex">#TXM5030</p>
+            <Button className="hidden md:flex">Completed</Button>
+            <h3>₹ 200</h3>
+            <Button color="blue">View</Button>
+          </div>
+          <div className="w-full h-[1px] bg-muted my-3"></div>
+           </div>
+          <div>
+           <div className="grid md:grid-cols-6 grid-cols-4 text-sm md:text-lg font-semibold text-muted  justify-items-center  items-center md:py-3 py-2">
+            <h2>Aman Gupta</h2>
+            <p>21 May 2024</p>
+            <p className="hidden md:flex">#TXM5030</p>
+            <Button className="hidden md:flex">Completed</Button>
+            <h3>₹ 200</h3>
+            <Button color="blue">View</Button>
+          </div>
+          <div className="w-full h-[1px] bg-muted my-3"></div>
+           </div>
+          <div>
+           <div className="grid md:grid-cols-6 grid-cols-4 text-sm md:text-lg font-semibold text-muted  justify-items-center  items-center md:py-3 py-2">
+            <h2>Aman Gupta</h2>
+            <p>21 May 2024</p>
+            <p className="hidden md:flex">#TXM5030</p>
+            <Button className="hidden md:flex">Completed</Button>
+            <h3>₹ 200</h3>
+            <Button color="blue">View</Button>
+          </div>
+          <div className="w-full h-[1px] bg-muted my-3"></div>
+           </div>
+          </div>
         </div>
+          <div>
+           <div className="grid md:grid-cols-6 grid-cols-4 text-sm md:text-lg font-semibold text-muted  justify-items-center  items-center md:py-3 py-2">
+            <h2>Aman Gupta</h2>
+            <p>21 May 2024</p>
+            <p className="hidden md:flex">#TXM5030</p>
+            <Button className="hidden md:flex">Completed</Button>
+            <h3>₹ 200</h3>
+            <Button color="blue">View</Button>
+          </div>
+          <div className="w-full h-[1px] bg-muted my-3"></div>
+           </div>
+          <div>
+           <div className="grid md:grid-cols-6 grid-cols-4 text-sm md:text-lg font-semibold text-muted  justify-items-center  items-center md:py-3 py-2">
+            <h2>Aman Gupta</h2>
+            <p>21 May 2024</p>
+            <p className="hidden md:flex">#TXM5030</p>
+            <Button className="hidden md:flex">Completed</Button>
+            <h3>₹ 200</h3>
+            <Button color="blue">View</Button>
+          </div>
+          <div className="w-full h-[1px] bg-muted my-3"></div>
+           </div>
       </div>
     </div>
   );
