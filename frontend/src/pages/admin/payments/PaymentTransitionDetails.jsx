a50a8a9ff50c6d67750665f6dc5899b7f4023a84
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-06-22 20:48:21 +0530

    style: style payment transition details page

--- frontend/src/pages/admin/payments/PaymentTransitionDetails.jsx
+++ frontend/src/pages/admin/payments/PaymentTransitionDetails.jsx
@@ -31,85 +31,94 @@ const PaymentTransitionDetails = () => {
         </Button>
       </div>
 
-      <div className="flex gap-2">
-        <div className="border border-gray-100 rounded-lg bg-white p-3 shadow-[0_0_5px_rgba(0,0,0,0.10)]">
-          <h1>Transaction Information</h1>
-          <div>
-            <div>
+      <div className="flex gap-4 mt-5">
+        <div className="border border-gray-100 rounded-lg bg-white p-5 shadow-[0_0_5px_rgba(0,0,0,0.10)] flex-1">
+          <h1 className="text-xl font-bold text-black/80">Transaction Information</h1>
+          <div className="flex flex-col gap-3 mt-4">
+            <div className="flex items-center justify-between">
                 <h3>Transition ID</h3>
                 <p>#TXN03423</p>
             </div>
-            <div>
+            <div className="flex items-center justify-between">
                 <h3>Booking ID</h3>
                 <p>#BKO03423</p>
             </div>
-            <div>
+            <div className="flex items-center justify-between">
                 <h3>Service Category</h3>
                 <p>Plumbing</p>
             </div>
-            <div>
+            <div className="flex items-center justify-between">
                 <h3>Payment Method</h3>
                 <p>UPI</p>
             </div>
-            <div>
+            <div className="flex items-center justify-between">
                 <h3>Payment Gateway Id</h3>
                 <p>PAYTM24234</p>
             </div>
-            <div>
+            <div className="flex items-center justify-between">
                 <h3>Transition Date & Time</h3>
                 <p>May 12,2024 at 10:20 PM</p>
             </div>
           </div>
         </div>
-        <div className="border border-gray-100 rounded-lg bg-white p-3 shadow-[0_0_5px_rgba(0,0,0,0.10)]">
-          <h1>Customer Details</h1>
+        <div className="border border-gray-100 rounded-lg bg-white p-5 shadow-[0_0_5px_rgba(0,0,0,0.10)] flex-[0.6]">
+          <h1 className="text-xl font-bold text-black/80">Customer Details</h1>
+          <div className="flex flex-col gap-3 mt-4">
           <UserInfo image='https://randomuser.me/api/portraits/women/34.jpg' name="Ram" id="#USR0934"/>
-          <div>
+          
+          <div className="flex items-center gap-2">
             <MdOutlinePhone size={20}/>
             <p>+91 75343 23452</p>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <MdOutlineEmail size={20}/>
             <p>ram343@gmail.com</p>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <CiLocationOn size={20}/>
             <p>Block A,Noida sector 2 </p>
           </div>
+          </div>
         </div>
-        <div className="border border-gray-100 rounded-lg bg-white p-3 shadow-[0_0_5px_rgba(0,0,0,0.10)]">
-          <h1>Provider Details</h1>
+        <div className="border border-gray-100 rounded-lg bg-white p-5 shadow-[0_0_5px_rgba(0,0,0,0.10)] flex-[0.6]">
+          <h1 className="text-xl font-bold text-black/80">Provider Details</h1>
+          <div className="flex flex-col gap-3 mt-4">
           <UserInfo image='https://randomuser.me/api/portraits/women/55.jpg' name="Manish" id="#PRO0934"/>
-           <div>
+           <div className="flex items-center gap-2">
             <MdOutlinePhone size={20}/>
             <p>+91 75343 23452</p>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <MdOutlineEmail size={20}/>
             <p>ram343@gmail.com</p>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <MdWorkOutline size={20}/>
             <p>5+ years of experience</p>
           </div>
+          </div>
         </div>
-        <div className="border border-gray-100 rounded-lg bg-white p-3 shadow-[0_0_5px_rgba(0,0,0,0.10)]">
-          <h1>Payment Breakdown</h1>
-          <div>
+        <div className="border border-gray-100 rounded-lg bg-white p-5 shadow-[0_0_5px_rgba(0,0,0,0.10)] flex-1">
+          <h1 className="text-xl font-bold text-black/80">Payment Breakdown</h1>
+          <div className="flex flex-col gap-3 mt-4">
+
+          
+          <div className="flex items-center justify-between">
             <h1>Service Amount</h1>
             <p>₹ 1,200</p>
           </div>
-          <div>
+          <div className="flex items-center justify-between">
             <h3>Platform Fee</h3>
             <p>₹ 100</p>
           </div>
-          <div>
+          <div className="flex items-center justify-between">
             <h3>Provider Earnings</h3>
             <p>₹ 1,100</p>
           </div>
-          <div>
+          <div className="flex items-center justify-between">
             <h3>Total Paid</h3>
             <p>₹ 1,200</p>
+          </div>
           </div>
         </div>
       </div>
