a12a7c7db521d6d72b57ce8d64d427065b8c19af
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-07-09 20:39:37 +0530

    feat : implement get all cities api

--- backend/src/controllers/state.controller.js
+++ backend/src/controllers/state.controller.js
@@ -23,68 +23,57 @@ const getAllStates = async (req, res) => {
   }
 };
 const getAllDistrictByStates = async (req, res) => {
-    try {
-    console.log("API HIT")
-        const { stateId } = req.params;
-       console.log(stateId);
-       
-        const AllDistricts = await districtModal.find({ stateId });
-        console.log(AllDistricts);
-        
-        if (AllDistricts.length === 0) {
-            return res.status(200).json({
-                status: true,
-                AllDistricts: [],
-                message: "Districts not found"
-            });
-        }
-        
-        return res.status(200).json({
-            status: true,
-            AllDistricts,
-            message: "Fetch all districts successfully"
-        });
+  try {
+    const { stateId } = req.params;
 
-    } catch (err) {
-
-        console.error(err);
-
-        return res.status(500).json({
-            message: "Internal server error"
-        });
-
+    const AllDistricts = await districtModal.find({ stateId });
+    if (AllDistricts.length === 0) {
+      return res.status(200).json({
+        status: true,
+        AllDistricts: [],
+        message: "Districts not found",
+      });
     }
+
+    return res.status(200).json({
+      status: true,
+      AllDistricts,
+      message: "Fetch all districts successfully",
+    });
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
-// const getAllCityByDistrict = async (req, res) => {
-//     try {
-   
-//         const { districtId } = req.params;
-       
-       
-//         const AllCities = await cityModal.find({ stateId });
+const getAllCityByDistrict = async (req, res) => {
+  try {
+    const { districtId } = req.params;
 
-//         if (AllCities.length === 0) {
-//             return res.status(200).json({
-//                 status: true,
-//                 AllCities: [],
-//                 message: "cities not found"
-//             });
-//         }
+    const allCities = await cityModal.find({ districtId });
 
-//         return res.status(200).json({
-//             status: true,
-//             AllCities,
-//             message: "Fetch all cities successfully"
-//         });
+    if (allCities.length === 0) {
+      return res.status(200).json({
+        status: true,
+        allCities: [],
+        message: "Cities not found",
+      });
+    }
 
-//     } catch (err) {
+    return res.status(200).json({
+      status: true,
+      allCities,
+      message: "Fetch all cities successfully",
+    });
+  } catch (err) {
+    console.error(err);
 
-//         console.error(err);
-
-//         return res.status(500).json({
-//             message: "Internal server error"
-//         });
-
-//     }
-// };
-module.exports = { getAllStates, getAllDistrictByStates };
+    return res.status(500).json({
+      status: false,
+      message: "Internal server error",
+    });
+  }
+};
+module.exports = { getAllStates, getAllDistrictByStates, getAllCityByDistrict };
--- backend/src/routes/state.routes.js
+++ backend/src/routes/state.routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getAllStates, getAllDistrictByStates } = require('../controllers/state.controller')
+const { getAllStates, getAllDistrictByStates, getAllCityByDistrict } = require('../controllers/state.controller')
 const validateObjectId = require('../middlewares/validateObjectId.middleware')
 
 
@@ -9,7 +9,7 @@ const router = express.Router()
 
 router.get('/states',getAllStates)
 router.get('/states/:stateId/districts',validateObjectId("stateId"),getAllDistrictByStates)
-router.get('/districts/:districtId/cities',validateObjectId,getAllDistrictByStates)
+router.get('/districts/:districtId/cities',validateObjectId('districtId'),getAllCityByDistrict)
 
 module.exports = router
 
