3e885fd93a1204256a6021346201bf84434db71f
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-07-16 09:26:07 +0530

    refactor(providers): create ProviderTableHeader component for table column labels

---
+++ frontend/src/pages/admin/providers/components/ProviderStatsSection.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import StatsCard from "../../../../components/common/admin/StatsCard";
+import { statsData } from "../data/statsData.jsx";
+
+const ProviderStatsSection = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-5 gap-2 mt-4 mb-6">
+      {statsData.map((item) => (
+        <StatsCard
+          key={item.id}
+          title={item.title}
+          value={item.value}
+          growth={item.growth}
+          icon={item.icon}
+          iconBg={item.iconBg}
+          iconColor={item.iconColor}
+          growthIcon={item.growthIcon}
+          growthColor={item.growthColor}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProviderStatsSection;
