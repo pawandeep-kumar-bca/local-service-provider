13c355e2c2c5a9f953e74fc1cd7f3a119b70dc99
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-07-28 06:48:24 +0530

    feat(review): add React Query mutation for creating reviews

---
+++ frontend/src/hooks/useReview.js
@@ -0,0 +1,15 @@
+import { useMutation } from "@tanstack/react-query"
+import { createReviews } from "../services/reviewService"
+
+
+export const useReview = ()=>{
+
+    const createReviewMutation = useMutation({
+        mutationFn:createReviews,
+        onError:(err)=>{
+            console.error('create review error:',err);
+            
+        }
+    })
+    return {createReviewMutation}
+} 
