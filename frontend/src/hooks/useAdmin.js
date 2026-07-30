a4d13442d80463f3b109f7aaa0cb2e9b92936c09
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-07-30 08:33:26 +0530

    feat: add admin bookings query hook

--- frontend/src/hooks/useAdmin.js
+++ frontend/src/hooks/useAdmin.js
@@ -1,26 +1,38 @@
-import { useQuery } from "@tanstack/react-query"
-import { getAllCategoriesByAdmin, getAllProvidersList, getAllUserList } from "../services/adminService"
+import { useQuery } from "@tanstack/react-query";
+import {
+  getAllBookingsByAdmin,
+  getAllCategoriesByAdmin,
+  getAllProvidersList,
+  getAllUserList,
+} from "../services/adminService";
 
+export const useUsers = (params = {}) => {
+  return useQuery({
+    queryKey: ["admin-users", params],
+    queryFn: () => getAllUserList(params),
+    enabled: true,
+  });
+};
 
-export const useUsers = (params = {})=>{
- return useQuery({
-    queryKey:['admin-users',params],
-    queryFn:()=>getAllUserList(params),
-    enabled:true
- })
-}
+export const useProviders = (params = {}) => {
+  return useQuery({
+    queryKey: ["admin-providers", params],
+    queryFn: () => getAllProvidersList(params),
+    enabled: true,
+  });
+};
+export const useCategoriesByAdmin = (params = {}) => {
+  return useQuery({
+    queryKey: ["admin-categories", params],
+    queryFn: () => getAllCategoriesByAdmin(params),
+    enabled: true,
+  });
+};
 
-export const useProviders = (params = {})=>{
-   return useQuery({
-      queryKey:['admin-providers',params],
-      queryFn:()=>getAllProvidersList(params),
-      enabled:true
-   })
-}
-export const useCategoriesByAdmin = (params = {})=>{
-   return useQuery({
-            queryKey:['admin-categories',params],
-            queryFn:()=>getAllCategoriesByAdmin(params),
-            enabled:true
-   })
-}
+export const useBookingsByAdmin = () => {
+  return useQuery({
+    queryKey: ["admin-bookings"],
+    queryFn: getAllBookingsByAdmin,
+    enabled: true,
+  });
+};
