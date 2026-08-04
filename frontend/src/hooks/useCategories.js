73ab71806de377c37c97122b616eb6f8806d5ce3
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-08-04 22:34:41 +0530

    feat : add get all category for tabs hook query

--- frontend/src/hooks/useCategories.js
+++ frontend/src/hooks/useCategories.js
@@ -1,17 +1,8 @@
 // hooks/categoryHooks.js
 
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { createCategory, getAllCategories } from "../services/categoryService";
+import { createCategory, getAllCategories, getAllCategoriesForTabs } from "../services/categoryService";
 import { useNavigate } from "react-router-dom";
-
-export const useCategories = (params = {}) => {
-  return useQuery({
-    queryKey: ["categories", params],
-    queryFn: () => getAllCategories(params),
-     enabled: true,  
-  });
-};
-
 export const useCategoryCreate = () => {
   const navigate = useNavigate();
   const createCategoryMutation = useMutation({
@@ -25,3 +16,19 @@ export const useCategoryCreate = () => {
   });
   return { createCategoryMutation };
 };
+
+export const useCategories = (params = {}) => {
+  return useQuery({
+    queryKey: ["categories", params],
+    queryFn: () => getAllCategories(params),
+     enabled: true,  
+  });
+};
+
+export const useCategoriesTabs = ()=>{
+  return useQuery({
+    queryKey:['categories-tabs'],
+    queryFn:getAllCategoriesForTabs,
+    enabled:true
+  })
+}
