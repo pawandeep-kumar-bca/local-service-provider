5839a8bb6b79febd4950217a7f63ae8f870431ef
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-08-10 19:54:46 +0530

    feat: add experience filter component

--- frontend/src/pages/user/filterComponents/ExperienceFilter.jsx
+++ frontend/src/pages/user/filterComponents/ExperienceFilter.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
+import React from "react";
 
-// const ExperienceFilter = ({ filters, setFilters }) => {
-//   const experience = ["1", "3", "5", "7", "10", "15"];
+const ExperienceFilter = ({ filters, setFilters }) => {
+  const experience = ["1", "3", "5", "7", "10", "15"];
 
-//   return (
-//     <select
-//       name="experience"
-//       id="experience"
-//       value={filters.experience}
-//       onChange={(e) =>
-//         setFilters((prev) => ({
-//           ...prev,
-//           experience: e.target.value,
-//         }))
-//       }
-//       className="border-2 border-brownness px-3 py-2 rounded-lg text-sm text-brownness font-semibold outline-none"
-//     >
-//       <option value="">Any Experience</option>
+  return (
+    <select
+      name="experience"
+      id="experience"
+      value={filters.experience}
+      onChange={(e) =>
+        setFilters((prev) => ({
+          ...prev,
+          experience: e.target.value,
+        }))
+      }
+      className="border-2 border-brownness px-3 py-2 rounded-lg text-sm text-brownness font-semibold outline-none"
+    >
+      <option value="">Any Experience</option>
 
-//       {experience.map((exp) => (
-//         <option key={exp} value={exp}>
-//           {exp}+ Years
-//         </option>
-//       ))}
-//     </select>
-//   );
-// };
+      {experience.map((exp) => (
+        <option key={exp} value={exp}>
+          {exp}+ Years
+        </option>
+      ))}
+    </select>
+  );
+};
 
-// export default ExperienceFilter;
+export default ExperienceFilter;
