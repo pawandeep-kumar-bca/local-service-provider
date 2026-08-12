a46dc74ec0edf521b15481dd2dfe927e96a19341
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-08-12 21:54:18 +0530

    feat :add useUser change password hook query

---
+++ frontend/src/hooks/useUser.js
@@ -0,0 +1,18 @@
+import { useMutation } from "@tanstack/react-query";
+import { changePassword } from "../services/userService";
+import { toast } from "react-toastify";
+
+export const useUser = () => {
+  const changePasswordMutation = useMutation({
+    mutationFn: changePassword,
+    onSuccess: (data) => {
+      toast.success(data?.message);
+    },
+    onError: (err) => {
+      toast.error(err?.response?.data?.message);
+      console.log(err.response?.data);
+    },
+  });
+
+  return { changePasswordMutation };
+};
