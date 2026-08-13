7861f572afca02b937db46869f8a96c93e5e9e20
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-08-13 10:08:31 +0530

    feat:add update profile service api

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -5,3 +5,8 @@ export const changePassword =async (payload)=>{
     const response= await api.patch('/users/change-password',payload)
     return response.data
 }
+
+export const updateProfile = async (payload)=>{
+    const response = await api.patch('/users/update-profile',payload)
+    return response.data
+}
