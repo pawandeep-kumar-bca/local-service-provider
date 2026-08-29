712234bada75960c4f49ea1e8a0f7bfb9ea99023
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-08-29 20:07:47 +0530

    fix: fix auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -7,11 +7,8 @@ async function tokenVerify(req, res, next) {
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "Token missing" });
     }
-
     const token = authHeader.split(" ")[1];
-      
-    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
     req.user = {
       id: decoded.id,
       role: decoded.role
