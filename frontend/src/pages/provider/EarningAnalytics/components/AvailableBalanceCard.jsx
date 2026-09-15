7255a6c8a6bff085636b0c1d033abf6fa6a485d7
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-09-15 19:07:59 +0530

    feat: update balance card

--- frontend/src/pages/provider/EarningAnalytics/components/AvailableBalanceCard.jsx
+++ frontend/src/pages/provider/EarningAnalytics/components/AvailableBalanceCard.jsx
@@ -4,6 +4,7 @@ import { CiBank } from "react-icons/ci";
 import Button from "../../../../components/common/Button";
 
 const AvailableBalanceCard = () => {
+  
   return (
     <div
       className=" bg-white rounded-xl border border-slate-100 p-5 shadow-[0_5px_20px_rgba(0,0,0,0.06)]
